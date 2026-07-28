da9614fd97f20821521d1e11ae54e1a2bab12160
Manash2005 <swainm099@gmail.com>
2026-07-28 13:33:44 +0530

    Update API base URL for production environment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL: "https://taskmanager-ti21.onrender.com",
+    baseURL: "https://taskmanager-re9u.onrender.com",
     withCredentials: true // allow cookies if backend sets them
 });
 
